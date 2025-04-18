5379e4830e8c4aa6ddfed7e1eaf9e291ec0a466a
zzimnii <happykeil@naver.com>
2025-04-18 11:38:42 +0900

    [fix] footer 이미지 호버 수정 #22

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,13 +1,16 @@
-const snsImg = document.querySelectorAll("img.img_sns")
-
-snsImg.forEach((img) => {
-    const origin = img.getAttribute("src");
-    const hover = origin.replace(".png", "_1.png");
-
-    img.addEventListener("mouseover", () => {
+document.addEventListener("includesLoaded", () => {
+    const snsImg = document.querySelectorAll("img.img_sns");
+  
+    snsImg.forEach((img) => {
+      const origin = img.getAttribute("src");
+      const hover = origin.replace(".png", "_1.png");
+  
+      img.addEventListener("mouseover", () => {
         img.src = hover;
-    });
-    img.addEventListener("mouseout", () => {
+      });
+  
+      img.addEventListener("mouseout", () => {
         img.src = origin;
+      });
     });
-});
+  });
